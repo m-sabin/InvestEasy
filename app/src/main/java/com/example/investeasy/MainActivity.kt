5547ba90911d37d634cdb12be4a78efbc2c42ae9
nibass <contato.ecomcenter@gmail.com>
2025-01-24 13:43:19 -0300

    Colocando margin nos botões
    Ajustes Finais

--- app/src/main/java/com/example/investeasy/MainActivity.kt
+++ app/src/main/java/com/example/investeasy/MainActivity.kt
@@ -11,7 +11,7 @@ import com.google.android.material.snackbar.Snackbar
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+        private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
